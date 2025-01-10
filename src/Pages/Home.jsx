3556af50c17a85fef8b320d0c5649ae5fbb508dc
Abdur Rahman <abdurrahman19011@gmail.com>
2025-01-10 13:21:05 +0600

    add .

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -29,7 +29,43 @@ export const Home = () => {
 
   return (
     <div>
-      <Carousel/>
+      <Carousel />
+      <div>
+        <div className="flex items-center py-3 sm:py-6 justify-center ">
+          <div className="flex w-full mx-2  rounded-md border border-gray-300 border-opacity-65 bg-gray-100 px-2">
+            {/* Input Field */}
+            <input
+              type="text"
+              className="flex w-full bg-transparent px-3 text-gray-700 rtl:text-right outline-none"
+              placeholder="Search Product..."
+            />
+
+            {/* Divider */}
+            <div className="border-l border-gray-400 border-opacity-70 my-1"></div>
+
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="relative rounded-full bg-transparent px-2 py-3"
+            >
+              <svg
+                className="w-6 h-6"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                  stroke="#999"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
 
       {/* Categories Section */}
       <section className="py-10 bg-gray-50">
